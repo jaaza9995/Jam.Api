f1835e1ffe160458fc66ff9bc476c574a6ba4f10
micned1111 <michael.n.i@icloud.com>
2025-11-28 16:49:51 +0100

    structured interfaces in frontend

--- JamReact/src/types/createStory.ts
+++ JamReact/src/types/createStory.ts
@@ -1,64 +1,81 @@
-export interface Story {
-  storyId: number;
-  title: string;
-  description: string;
-  difficultyLevel: number;
-  accessibility: number;
-  questionCount: number;
-  code?: string;
-  lastPlayed: string;
-  //userId: string;
-}
-export interface EndingDto {
-  goodEnding: string;
-  neutralEnding: string;
-  badEnding: string;
-}
-
-export interface EndingErrors {
-  good: string;
-  neutral: string;
-  bad: string;
-}
-
+// ---- INTRO CREATION DTO ----
 export interface IntroDto {
-  title: string;
-  description: string;
-  introText: string;
-  difficultyLevel: number;
-  accessibility: number;
+	title: string;
+	description: string;
+	introText: string;
+	difficultyLevel: number;
+	accessibility: number;
 }
 
 export interface IntroErrors {
-  title: string;
-  description: string;
-  introText: string;
-  difficulty: string;
-  accessibility: string;
+	title: string;
+	description: string;
+	introText: string;
+	difficulty: string;
+	accessibility: string;
 }
+
 
 // ---- QUESTION CREATION DTO ----
 export interface AnswerOptionDto {
-  answerText: string;
-  contextText: string;
+	answerText: string;
+	contextText: string;
 }
 
 export interface QuestionSceneDto {
-  storyText: string;
-  questionText: string;
-  correctAnswerIndex: number;
-  answers: AnswerOptionDto[];
+	storyText: string;
+	questionText: string;
+	correctAnswerIndex: number;
+	answers: AnswerOptionDto[];
 }
 
 export interface QuestionScenesPayload {
-  questionScenes: QuestionSceneDto[];
+	questionScenes: QuestionSceneDto[];
 }
 
-// ---- ERRORS FOR CREATE QUESTIONS ----
 export interface QuestionErrors {
-  storyText?: string;
-  questionText?: string;
-  answers?: string;
-  contextTexts?: string;
-  correct?: string;
+	storyText?: string;
+	questionText?: string;
+	answers?: string;
+	contextTexts?: string;
+	correct?: string;
+}
+
+
+// ---- ENDING CREATION DTO ----
+export interface EndingDto {
+	goodEnding: string;
+	neutralEnding: string;
+	badEnding: string;
+}
+
+export interface EndingErrors {
+	good: string;
+	neutral: string;
+	bad: string;
+}
+
+
+// ---- STORY CREATION DTO ----
+export interface StoryCreationData {
+    intro: {
+        title: string;
+        description: string;
+        difficulty: string;
+        accessibility: string;
+        introText: string;
+    };
+    questions: QuestionSceneDto[];
+    endings: {
+        good: string;
+        neutral: string;
+        bad: string;
+    };
+    token?: string;
+}
+
+export interface StoryCreationContextType {
+    data: StoryCreationData;
+    setData: React.Dispatch<React.SetStateAction<StoryCreationData>>;
+    API_URL: string;
 }
