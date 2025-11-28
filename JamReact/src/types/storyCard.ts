72b1038156726ed0a66a8b807b1052cdfed33c5e
micned1111 <michael.n.i@icloud.com>
2025-11-28 16:45:09 +0100

    moved logic from converting accessibility and difficultyLevel from backend to frontend for consistency

--- JamReact/src/types/storyCard.ts
+++ JamReact/src/types/storyCard.ts
@@ -1,9 +1,9 @@
 export interface StoryCard {
-  storyId: number;
-  title: string;
-  description: string;
-  questionCount: number;
-  difficultyLevel: string;   // backend sender string når vi bruker ToString()
-  accessibility: string;     // backend sender string når vi bruker ToString()
-  code: string;
+	storyId: number;
+	title: string;
+	description: string;
+	questionCount: number;
+	difficultyLevel: number;
+	accessibility: number;
+	code: string;
 }
--- api/DTOs/Story/StoryCardDto.cs
+++ api/DTOs/Story/StoryCardDto.cs
@@ -1,12 +1,14 @@
+using Jam.Api.Models.Enums;
+
 namespace Jam.Api.DTOs.Story;
 
-public class StoryCardDto //keep
+public class StoryCardDto
 {
     public int StoryId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int QuestionCount { get; set; }
-    public string DifficultyLevel { get; set; } = string.Empty;
-    public string Accessibility { get; set; } = string.Empty;
-    public string Code { get; set; } = string.Empty;  // Viktig!
+    public DifficultyLevel DifficultyLevel { get; set; }
+    public Accessibility Accessibility { get; set; }
+    public string Code { get; set; } = string.Empty;
 }
